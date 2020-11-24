c53d86421374eae834b824f558a9d7a6a87d1f0e
ch <mkmsacc@outlook.com>
2020-11-24 20:36:50 +0700

    Add Country rest

--- src/main/java/com/example/springboottest/SpringBootTestApplication.java
+++ src/main/java/com/example/springboottest/SpringBootTestApplication.java
@@ -1,13 +1,37 @@
 package com.example.springboottest;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+
+import com.example.springboottest.beans.FlightBuilder;
+import com.example.springboottest.model.Country;
+import com.example.springboottest.model.CountryRepository;
 
 @SpringBootApplication
+@Import(FlightBuilder.class)
 public class SpringBootTestApplication {
+
+	@Autowired
+	private Map<String, Country> countriesMap;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootTestApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner configureRepository(
+			CountryRepository countryRepository) {
+		return args -> {
+			for (Country country : countriesMap.values()) {
+				countryRepository.save(country);
+			}
+		};
 	}
 
 }
---
+++ src/main/java/com/example/springboottest/model/CountryController.java
@@ -0,0 +1,19 @@
+package com.example.springboottest.model;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CountryController {
+
+	@Autowired
+	private CountryRepository repository;
+
+	@GetMapping("/countries")
+	List<Country> findAll() {
+		return repository.findAll();
+	}
+}
